1172e1c82c7a5faa4c4f0809e9a13f92a9696b17
VladimirGromyko <vl-mailbox@mail.ru>
2026-01-03 01:57:19 +0300

    changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
       '@': resolve(__dirname, 'src'),
     },
   },
-  publicPath: '/form-generator/'
+  // publicPath: '/form-generator/'
+  base: '/form-generator/'
 })
